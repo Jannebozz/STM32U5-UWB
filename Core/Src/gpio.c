47cd3151cee12b8ebb228512b37765394909e441
Anders Jansson <hans.anders.jansson@gmail.com>
2024-08-27 09:41:37 +0200

    Changed callback name

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -140,7 +140,7 @@ void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 2 */
-void HAL_GPIO_EXTI_Callback( uint16_t GPIO_Pin )
+void HAL_GPIO_EXTI_Rising_Callback( uint16_t GPIO_Pin )
 {
 	if (GPIO_Pin == SR1020_INT_Pin)
 	{
